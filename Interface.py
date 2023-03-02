dce041bc4551cb498562e2381e0ba505e0bff75a
Rookit15 <attehiltunen@hotmail.com>
2023-03-02 19:11:05 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -3,6 +3,7 @@ import sqlite3
 db = sqlite3.connect('game_database.sqlite')
 cur = db.cursor()
 
+
 def initializeDB():
     try:
         f = open("create_database.sql", "r")
@@ -15,37 +16,39 @@ def initializeDB():
     except:
         print("No SQL file to be used for initialization")
 
-    
-
 
 def main():
     initializeDB()
     userInput = -1
     while (userInput != "0"):
-        print("\nMenu options:")
+        print("\nMenu options: (1-5 are queries)")
         print("1: ")
-        print("2: ")
+        print("2: See all rooms player have cleared.")
         print("3: ")
         print("4: ")
         print("5: ")
-        print("6: ")
+        print("6: Search players stats with name.")
+        print("7: Insert, update or delete items")
         print("0: Quit")
         userInput = input("What do you want to do? ")
         print(userInput)
         if userInput == "1":
-            searchPlayerStats()
-        if userInput == "2":
-            clearedRooms()
-        if userInput == "3":
-            pass
-        if userInput == "4":
-            pass
-        if userInput == "5":
 
-        if userInput == "6":
-            pass
-        if userInput == "0":
+        elif userInput == "2":
+            clearedRooms()
+
+        elif userInput == "3":
+
+        elif userInput == "4":
+
+        elif userInput == "5":
+
+        elif userInput == "6":
+            searchPlayerStats()
+        elif userInput == "0":
             print("Ending software...")
+        else:
+            print("Didn't quite get that.\nGive your choice again.")
     db.close()
     return
 
@@ -68,4 +71,18 @@ def clearedRooms():
         print(i, "\n")
     return
 
-main()
+
+def modifyData():
+    player_name = input("Give player name: ")
+    choice = -1
+    while (choice != 0):
+        choice = input('''What you want to do?
+            1) Insert new item
+            2) Update old item to new
+            3) Delete item''')
+        if choice == 1:
+            Weapon = input('''What weapon you want to use?
+                1) Iron Sword
+                2) Claw Dagger
+                3) Spectral Armor
+                your choice: ''')
