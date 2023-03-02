e9b5a42e800e3c4b7fcf5dc44e84b36b8b7d3b97
Rookit15 <attehiltunen@hotmail.com>
2023-03-02 20:07:10 +0200

    Interface functions and first working queries

--- Interface.py
+++ Interface.py
@@ -31,20 +31,21 @@ def main():
         print("7: Insert, update or delete items")
         print("0: Quit")
         userInput = input("What do you want to do? ")
-        print(userInput)
-        if userInput == "1":
 
+        if userInput == "1":
+            pass
         elif userInput == "2":
             clearedRooms()
-
         elif userInput == "3":
-
+            pass
         elif userInput == "4":
-
+            pass
         elif userInput == "5":
-
+            pass
         elif userInput == "6":
             searchPlayerStats()
+        elif userInput == "7":
+            modifyData()
         elif userInput == "0":
             print("Ending software...")
         else:
@@ -55,34 +56,42 @@ def main():
 
 def searchPlayerStats():
     player_name = input("Give player name: ")
-    cur.execute(f"SELECT * FROM Player WHERE Player_name='?';", (player_name,))
+    cur.execute("SELECT * FROM Players WHERE Name=?;", [player_name])
     result = cur.fetchone()
+    if result == None:
+        print("No such player.")
+        return
     for i in result:
-        print(i, "\n")
+        print(i)
     return
 
 
 def clearedRooms():
-    player_name = input("Give player name: ")
     cur.execute(
-        "SELECT * FROM Rooms r INNER JOIN Player p ON r.level < p.level WHERE p.name='?';", (player_name,))
+        "SELECT Players.Name, GROUP_CONCAT(Rooms.Name,',') FROM Players JOIN Rooms ON Players.Level<Rooms.Level GROUP BY Players.Name;")
     result = cur.fetchall()
+
     for i in result:
         print(i, "\n")
     return
 
 
 def modifyData():
-    player_name = input("Give player name: ")
-    choice = -1
-    while (choice != 0):
-        choice = input('''What you want to do?
-            1) Insert new item
-            2) Update old item to new
-            3) Delete item''')
-        if choice == 1:
-            Weapon = input('''What weapon you want to use?
-                1) Iron Sword
-                2) Claw Dagger
-                3) Spectral Armor
-                your choice: ''')
+    print("Here you can insert, update or delete players.")
+    choice = input(''''What you want to do?
+        1) Insert new player
+        2) Update existing player
+        3) Delete existing player
+        your choice: ''')
+    if choice == "1":
+        player_name = input("Player name: ")
+        cur.execute("SELECT COUNT(*) FROM Players;")
+        count = cur.fetchone()
+        try:
+            cur.execute("INSERT INTO Players(PlayerId,Name) VALUES (?,?);",
+                        (count[0]+1, player_name))
+        except:
+            print("Your name needs to be atleast 3 characters long.")
+
+
+main()
